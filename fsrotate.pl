d9d1a9d5d8ba007e924919727c121810d7c26286
Maxim Sviridenko <maxim@emrk.ru>
2018-08-30 23:22:10 +0700

    Add sync function

--- fsrotate.pl
+++ fsrotate.pl
@@ -21,15 +21,20 @@
 use strict;
 use warnings;
 use utf8;
+use File::Copy;
 
-use constant DEBUG => 1;
+use constant DEBUG => 0;
 
 my $dlist = 'info.txt'; #File of direcory listing
 my $files_limit = 1; #Count of files for reduce
+my $sync_dir = "YandexDisk/DB/"; #Directory for sync
+my $syncfl = $ARGV[0] || ''; #Set flag to sync
 
 my @struct = ();
 my @dirs = ();
 my @files = ();
+my $source_dir = ''; #Directory source of sync fiels
+my $sync_file = '';
 
 print "Warning! Debug mode\n" if DEBUG;
 
@@ -46,12 +51,22 @@ foreach my $key (@struct){
 	};
 };
 
-foreach my $key (@dirs){
-	print "\nChange directory to $key\n";
-	if (chdir $key) {
+#Clear sync dir
+if(!DEBUG && $syncfl){
+	chdir $sync_dir;
+	unlink <*>;
+	print "Clear synd dir is Ok\n";
+	chdir "../../";
+};
+
+foreach my $dir (@dirs){
+	print "\nChange directory to $dir\n";
+	if (chdir $dir) {
 		@files  = <*>;
+		
+		#Deleting files from directory
 		if (@files > $files_limit){
-			splice (@files, -1*$files_limit);
+			$sync_file = splice (@files, -1*$files_limit);
 
 			foreach my $key (@files){
 				print "Unlink file: $key ...";
@@ -59,10 +74,22 @@ foreach my $key (@dirs){
 				print "done\n";
 			};
 		};
+		
+		$source_dir = $dir;
+
 		#Set path to root
-		$key=~s/(\w+)/../g;
-		print "Change dir to $key\n" if DEBUG;
-		chdir $key;
+		$dir=~s/(\w+)/../g;
+		print "Change dir to $dir\n" if DEBUG;
+		chdir $dir;
+
+		if($syncfl && $sync_file){
+			print "Sync file: $sync_file\n";
+			if(!DEBUG){
+				my $tstamp = time;
+				copy ($source_dir.$sync_file, $sync_dir.$tstamp."_".$sync_file) || die "Can't copy file: $sync_file";
+			};
+			$sync_file = '';
+		};
 	}else{
 		print "Can't change directory\n";	
 
